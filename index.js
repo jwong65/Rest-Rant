73a0ecf45f0d203c06050093ca0469ded83e76b8
jwong65 <jason.wong6507@gmail.com>
2022-12-13 21:35:25 -0500

    Added the environment variable for the port instead of read

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+require(`dotenv`).config()
+
+app.get('/', (req, res)=>{
+    res.send('This is the first page')
+})
+
+//Use this to get any page that doesn't reach any path
+app.get('*',(req,res)=>{
+    res.status(404).send("<h1>THIS IS A 404</h1>")
+})
+
+app.listen(process.env.PORT)
