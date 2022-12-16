3cfe5f2c1cc4b57cf349ef9301a03d6cd056f67f
jwong65 <jason.wong6507@gmail.com>
2022-12-15 21:12:50 -0500

    Added the format for places.js and had the format being inserted with just the H1 for now

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,11 +1,21 @@
 const React =  require('react')
 const Def = require('../default')
 
-function places(){
+function places(placesinfo){
+    let placeFormat =placesinfo.places.map((place)=>{
+        return(
+            <Def>
+                <main>
+                    <h1>Places</h1>
+                </main>
+            </Def>
+        )
+    })
     return(
         <Def>
             <main>
-                <h1>Places</h1>
+                <h1> Plaecs Index Page</h1>
+                {placeFormat}
             </main>
         </Def>
     )
