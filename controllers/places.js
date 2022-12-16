12fc4efe4e9f7e373764249da566a5d7c23fab0f
jwong65 <jason.wong6507@gmail.com>
2022-12-15 21:00:31 -0500

    Created another directory for places.jsx and had the controller render it

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 
 router.get('/', (req, res)=>{
-    res.send('GET /places')
+    //res.send('GET /places')
+
+    res.render('places/index')
 })
 module.exports = router
