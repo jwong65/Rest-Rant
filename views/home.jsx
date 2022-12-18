940fab6bd0924a3834b5759de7e0def1da4dd6bf
jwong65 <jason.wong6507@gmail.com>
2022-12-18 11:42:41 -0500

    Added image and styled it with CSS

--- views/home.jsx
+++ views/home.jsx
@@ -7,6 +7,9 @@ function home(){
         <Def>
             <main>
                 <h1>Home</h1>
+                <div>
+                    <img src="/images/allec-gomes-strawberry-unsplash.jpg"></img>
+                </div>
                 {/* Button that sends you to the places route */}
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
