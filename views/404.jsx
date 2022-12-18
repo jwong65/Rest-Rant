210a9de54281cdcce79620574448c347a0fa4a78
jwong65 <jason.wong6507@gmail.com>
2022-12-18 11:53:23 -0500

    Added attribute to the image for 404

--- views/404.jsx
+++ views/404.jsx
@@ -9,6 +9,9 @@ function error404(){
                 <p> We cannot find this page</p>
                 <div>
                     <img src="images/jason-hogan-freedom-unsplash.jpg" alt='freedom' className='mountainpic'></img>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@jasonhogan?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Hogan</a> on <a href="https://unsplash.com/s/photos/freedom?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
                 </div>
             </main>
         </Def>
