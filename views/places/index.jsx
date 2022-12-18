4eb9efe2197e1802109886ef2593da15d48fb44e
jwong65 <jason.wong6507@gmail.com>
2022-12-18 12:17:18 -0500

    Added bootstrap that will make rows

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,14 +5,14 @@ function places(placesinfo){
     let placeFormat =placesinfo.places.map((place)=>{
         return(
             <Def>
-                <main>
+                <div className='col-sm-6'>
                     {/* <h1>Places</h1> */}
                     <h2>{place.name}</h2>
                     <img src = {place.pic} alt = {place.name} className={place.city}></img>
                     <div>
                         Photo by <a href={place.attribute}>{place.author}</a> on <a href={place.unsplash}>Unsplash</a>
                     </div>
-                </main>
+                </div>
             </Def>
         )
     })
@@ -20,7 +20,10 @@ function places(placesinfo){
         <Def>
             <main>
                 <h1> Plaecs Index Page</h1>
-                {placeFormat}
+                <div className='row'>
+                    {placeFormat}
+                </div>
+                
             </main>
         </Def>
     )
