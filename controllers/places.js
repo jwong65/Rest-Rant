3cfed0adb678e76eb986894fc3445a83bd1e1a30
jwong65 <jason.wong6507@gmail.com>
2022-12-19 16:22:16 -0500

    Added new directoru for models, moved the array of places into places.js in models

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,5 @@
 const router = require('express').Router()
-let places = [{
-  name: 'H-Thai-ML',
-  city: 'Seattle',
-  state: 'WA',
-  cuisines: 'Thai, Pan-Asian',
-  pic: 'images/ben-dutton-market-unsplash.jpg',
-  author: `Ben Dutton`,
-  attribute: `https://unsplash.com/@benjamiindutton?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
-  unsplash: `https://unsplash.com/s/photos/seattle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`
-  //Photo by <a href="https://unsplash.com/@benjamiindutton?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ben Dutton</a> on <a href="https://unsplash.com/s/photos/seattle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-
-}, {
-  name: 'Coding Cat Cafe',
-  city: 'Phoenix',
-  state: 'AZ',
-  cuisines: 'Coffee, Bakery',
-  pic: 'images/rylan-hill-phoenix-unsplash.jpg',
-  author: `Rylan Hill`,
-  attribute: `https://unsplash.com/@rylanhill?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
-  unsplash: `https://unsplash.com/s/photos/phoenix?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`
-  //Photo by <a href="https://unsplash.com/@rylanhill?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Rylan Hill</a> on <a href="https://unsplash.com/s/photos/phoenix?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-
-}]
+const places = require('../models/places')
 router.post('/', (req, res)=>{
   console.log(req.body)
   res.send('POST /places')
