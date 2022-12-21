51f2982d1a7be20b267d0b331ac219473facd530
jwong65 <jason.wong6507@gmail.com>
2022-12-20 21:32:39 -0500

    updated show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,41 @@
 const React = require('react')
 const Def = require('../default')
+const places = require('../../models/places')
 
-function show(){
+// data argument includes place information
+function show(data){
     return(
         <Def>
             <main>
-                <h1> Show this Page</h1>
+            <img src= {data.place.pic} alt={data.place.name}/>
+                <h1>
+                    {data.place.name}
+                </h1>
+                <h2>
+                    Reviews
+                </h2>
+                <h3>Description</h3>
+                <p>
+                    Located in {data.place.city}
+                </p>
             </main>
+            <div>
+                <h3>Comments</h3>
+            </div>
         </Def>
     )
 }
+
+// name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: 'images/ben-dutton-market-unsplash.jpg',
+//     author: `Ben Dutton`,
+//     attribute: `https://unsplash.com/@benjamiindutton?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
+//     unsplash: `https://unsplash.com/s/photos/seattle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
+//     site: 'Unsplash'
+//     //Photo by <a href="https://unsplash.com/@benjamiindutton?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ben Dutton</a> on <a href="https://unsplash.com/s/photos/seattle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
 
 module.exports=show
