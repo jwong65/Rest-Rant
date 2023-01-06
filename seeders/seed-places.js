14961c7dd4487b4fd82ed3d097ed65fa70427603
jwong65 <jason.wong6507@gmail.com>
2023-01-06 14:59:06 -0500

    Added the array from previously places.js

--- seeders/seed-places.js
+++ seeders/seed-places.js
@@ -1,1 +1,28 @@
 const db = require('./models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/ben-dutton-market-unsplash.jpg',
+    author: `Ben Dutton`,
+    attribute: `https://unsplash.com/@benjamiindutton?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
+    unsplash: `https://unsplash.com/s/photos/seattle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
+    site: 'Unsplash',
+    founded: 1989
+    
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/rylan-hill-phoenix-unsplash.jpg',
+    author: `Rylan Hill`,
+    attribute: `https://unsplash.com/@rylanhill?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
+    unsplash: `https://unsplash.com/s/photos/phoenix?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`,
+    site: 'Unsplash',
+    founded: 2020
+  
+  }]
+)
